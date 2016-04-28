ab5a261d5922b146bc05b03261d832457ef6d663
Janne Koskinen <jtjk@iki.fi>
2016-04-28 11:04:16 +0300

    init

---
+++ app/src/test/java/jtjk/iki/fi/dublindemo/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package jtjk.iki.fi.dublindemo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
