2e73bdd0e93ad113043a30b02d5b99c808657f29
Janne Koskinen <jtjk@iki.fi>
2016-04-28 12:16:07 +0300

    notifications

--- app/src/main/java/jtjk/iki/fi/dublindemo/MainActivity.java
+++ app/src/main/java/jtjk/iki/fi/dublindemo/MainActivity.java
@@ -4,13 +4,53 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Intent;
+import android.util.Log;
+import android.webkit.CookieManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.microsoft.windowsazure.notifications.NotificationsManager;
 
 public class MainActivity extends AppCompatActivity {
+    public static MainActivity mainActivity;
+    public static Boolean isVisible = false;
+    private GoogleCloudMessaging gcm;
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+
+    class MyWebViewClient extends WebViewClient {
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if(!url.contains("dublinwebdemo.azurewebsites.net")){
+                return super.shouldOverrideUrlLoading(view, url);
+            } else {
+                view.loadUrl(url);
+                return true;
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainActivity = this;
+        NotificationsManager.handleNotifications(this, NotificationSettings.SenderId, MyHandler.class);
+        registerWithNotificationHubs();
+
+        WebView browser = (WebView) findViewById(R.id.webView);
+        CookieManager.getInstance().setAcceptCookie(true);
+        browser.getSettings().setJavaScriptEnabled(true);
+        browser.setWebViewClient(new MyWebViewClient());
+
+        browser.loadUrl("http://dublinwebdemo.azurewebsites.net/");
+        CookieManager.getInstance().setAcceptCookie(true);
     }
 
     @Override
@@ -35,4 +75,66 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private boolean checkPlayServices() {
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int resultCode = apiAvailability.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (apiAvailability.isUserResolvableError(resultCode)) {
+                apiAvailability.getErrorDialog(this, resultCode, PLAY_SERVICES_RESOLUTION_REQUEST)
+                        .show();
+            } else {
+                Log.i("TAG", "This device is not supported by Google Play Services.");
+                ToastNotify("This device is not supported by Google Play Services.");
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    public void registerWithNotificationHubs()
+    {
+        Log.i("TAG", " Registering with Notification Hubs");
+
+        if (checkPlayServices()) {
+            // Start IntentService to register this application with GCM.
+            Intent intent = new Intent(this, RegistrationIntentService.class);
+            startService(intent);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        isVisible = true;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        isVisible = false;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        isVisible = true;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        isVisible = false;
+    }
+
+    public void ToastNotify(final String notificationMessage) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this, notificationMessage, Toast.LENGTH_LONG).show();
+                TextView helloText = (TextView) findViewById(R.id.text_hello);
+                helloText.setText(notificationMessage);
+            }
+        });
+    }
 }
---
+++ app/src/main/java/jtjk/iki/fi/dublindemo/NotificationSettings.java
@@ -0,0 +1,10 @@
+package jtjk.iki.fi.dublindemo;
+
+/**
+ * Created by jtjk on 27.4.16.
+ */
+public class NotificationSettings {
+    public static String SenderId = "103013735467";
+    public static String HubName = "dublinhub2";
+    public static String HubListenConnectionString = "Endpoint=sb://dublinhub2.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=gXEzStDAXKBYFl6gJiuWQuo87JjFwOihqtIK+E4S5/g=";
+}
